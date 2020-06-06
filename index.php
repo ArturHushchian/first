952bdaac97abbd2140598a7340247cede9b7250b
Artur <arturovikovich@gmail.com>
2020-06-06 12:17:52 +0300

    code stayling

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
     $timeZone = new DateTimeZone('Europe/Kiev');
     $dateObject = new DateTime();
+
     $dateObject->setTimezone($timeZone);
-    
+
 
 
      echo 'good morning, time is ' .  $dateObject->format('H:i:s');
